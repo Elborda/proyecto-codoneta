2e233dbdaf04fdb6a51ee03d8c9ed5e50a93c2bc
Mauro Roth <maurogroth@gmail.com>
2023-04-13 19:26:09 -0300

    fixture 3, acortando el código del script

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let clavePersonal = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+let clavePersonal = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 let requestOptions = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": clavePersonal,
+    "x-rapidapi-key":  clavePersonal,
     "x-rapidapi-host": "v3.football.api-sports.io",
   },
   redirect: "follow",
@@ -12,38 +12,28 @@ const fixtures = document.getElementById('fixture');
 const ol = document.createElement('ol');
 
 //fetch("https://v3.football.api-sports.io/fixtures?live=all", requestOptions)
-fetch("datos.json")
+fetch('datos.json')
     .then((res) => res.json())
     .then((data) => {
         let cantidadResponse = data.response.length;
         console.log(data, cantidadResponse);
-        
-        for(let i = 0; i < cantidadResponse; i++){
-            let fixtureReferee = data.response[i].fixture.referee;
-            let fixtureDate = data.response[i].fixture.date; 
-            let fixtureTimeZone = data.response[i].fixture.timezone;
-            let fixtureVenueName = data.response[i].fixture.venue.name; 
-            let fixtureVenueCity = data.response[i].fixture.venue.city;
-            let leagueName = data.response[i].league.name; 
-            let leagueCountry = data.response[i].league.country;
-            let teamsHomeName = data.response[i].teams.home.name;
-            let teamsAwayName = data.response[i].teams.away.name;
-            let goalsHome = data.response[i].goals.home;
-            let goalsAway = data.response[i].goals.away;
+        for(let i = 0; i < cantidadResponse; i++){ 
             const li = document.createElement('li');
             li.innerHTML = `<h3>
-                            Liga: ${leagueName}<br>
-                            País: ${leagueCountry}<br>
-                            Estadio: ${fixtureVenueName}<br>
-                            Ciudad: ${fixtureVenueCity}<br>
-                            Local: ${teamsHomeName} - Goles: ${goalsHome}<br>
-                            Visitante: ${teamsAwayName} - Goles: ${goalsAway}<br>
-                            Árbitro: ${fixtureReferee}<br>
-                            Fecha y Hora: ${fixtureDate} ${fixtureTimeZone}<br>
-                            </h3>`;
+                Liga: ${data.response[i].league.name}<br>
+                País: ${data.response[i].league.country}<br>
+                Estadio: ${data.response[i].fixture.venue.name}<br>
+                Ciudad: ${data.response[i].fixture.venue.city}<br>
+                Local: ${data.response[i].teams.home.name} 
+                      - Goles: ${data.response[i].goals.home}<br>
+                Visitante: ${data.response[i].teams.away.name} 
+                      - Goles: ${data.response[i].goals.away}<br>
+                Árbitro: ${data.response[i].fixture.referee}<br>
+                Fecha y Hora: ${data.response[i].fixture.date} 
+                              ${data.response[i].fixture.timezone}<br>
+                </h3>`;
             ol.appendChild(li);
         }
     })
   .catch((error) => console.log("error", error));
-
 fixtures.appendChild(ol);
